acd3f5144d8b1d019ccbd509e42eb8dbbf528a49
hillary-b <31204891+hillary-b@users.noreply.github.com>
2019-08-23 16:19:01 +0100

    Update ticket_form.php

--- ticket_form.php
+++ ticket_form.php
@@ -1,12 +1,18 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <link rel="stylesheet" type="text/css" href="">
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-  </head>
+<?php
 
-<body>
+if(isset($_POST['submit'])){
+  $name = $_POST['name'];
+  $accountNum = $_POST['accountNum'];
+  $sortCode = $_POST['sortCode'];
+  $expiryDate = $_POST['expiryDate'];
+  $numOfTickets = $_POST['numOfTickets'];
+  
+  $mailTo = "hbunketi@gmail.com";
+  $headers = "From: ".$mailFrom;
+  $txt = $numOfTickets." tickets have been bought by ".$name.".\n\n";
+  
+  mail($mailTo, "new ticket purchase", $txt, $headers);
+  header("Location: index.php?mailsend");
+}
 
-</body>
-</html>
+?>
